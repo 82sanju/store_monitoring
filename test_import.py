5a2c2b78fea978512410a7900a826eb3f408ed74
Sanju Preetham <59658774+82sanju@users.noreply.github.com>
2024-11-09 17:06:59 +0530

    Add files via upload

---
+++ test_import.py
@@ -0,0 +1,7 @@
+import fastapi
+import sqlalchemy
+import psycopg2
+
+print("fastapi:", fastapi.__version__)
+print("sqlalchemy:", sqlalchemy.__version__)
+print("psycopg2-binary imported successfully!")
